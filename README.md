67d352c23089ee11514ff70bcab95a2bb9e5198b
Johnny Hausman <johnny@appdevdesigns.net>
2015-10-14 16:14:42 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+opstool-donations ![Build Status](https://travis-ci.org/github.com/0.png)
+================
+
+A new plugin.
---
+++ config/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Routes
+ *
+ * Use this file to add any module specific routes to the main Sails
+ * route object.
+ */
+
+
+module.exports = {
+
+
+  /*
+
+  '/': {
+    view: 'user/signup'
+  },
+  '/': 'opstool-donations/PluginController.inbox',
+  '/': {
+    controller: 'opstool-donations/PluginController',
+    action: 'inbox'
+  },
+  'post /signup': 'opstool-donations/PluginController.signup',
+  'get /*(^.*)' : 'opstool-donations/PluginController.profile'
+
+  */
+
+
+};
+
